3fe5726cfc979967dc00ad2a5a7a2ec3f262bd02
brahimjarallah <brahim.jarallah@gmail.com>
2021-08-19 21:44:29 +0100

    exercice tracker updated

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 require("dotenv").config()
 const mongoose = require("mongoose")
+
+app.use(cors())
+app.use(express.static("public"))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
 let uri =
   "mongodb+srv://brahim:" +
   process.env.PW +
@@ -18,40 +25,25 @@ let exerciseSessionSchema = new mongoose.Schema({
 })
 let userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  log: [exerciseSessionSchema],
 })
 let Session = mongoose.model("Session", exerciseSessionSchema)
 let User = mongoose.model("User", userSchema)
-// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post(
   "/api/users",
   bodyParser.urlencoded({ extended: false }),
   (req, res) => {
-    // console.log(req.body.username)
-    // res.json({ created: true, username: req.body.username, id: req.body.id})
-
     let newUser = new User({ username: req.body.username })
     newUser.save((error, data) => {
       if (!error) {
-        console.log(data.username)
         let respObj = {}
         respObj["username"] = data.username
         respObj["_id"] = data.id
         res.json(respObj)
-      } else {
-        res.json({ error: "error found!" })
-        console.log(error)
       }
     })
   }
 )
-
-app.use(cors())
-app.use(express.static("public"))
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port)

131142576aa31cbf5ade5ee706b4bdb8793ad920
brahimjarallah <brahim.jarallah@gmail.com>
2021-08-19 21:29:17 +0100

    exercice tracker updated

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const cors = require("cors")
 require("dotenv").config()
 const mongoose = require("mongoose")
 let uri =
-  "mongodb+srv://brahim" +
+  "mongodb+srv://brahim:" +
   process.env.PW +
-  "<password>@cluster0.nab8p.mongodb.net/db1?retryWrites=true&w=majority"
+  "@cluster0.nab8p.mongodb.net/db1?retryWrites=true&w=majority"
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let exerciseSessionSchema = new mongoose.Schema({
@@ -16,13 +17,35 @@ let exerciseSessionSchema = new mongoose.Schema({
   date: String,
 })
 let userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  log: [exerciseSessionSchema]
-
+  username: { type: String, required: true },
+  log: [exerciseSessionSchema],
 })
-let Session = mongoose.model('Session', exerciseSessionSchema)
-let User = mongoose.model('User', userSchema)
+let Session = mongoose.model("Session", exerciseSessionSchema)
+let User = mongoose.model("User", userSchema)
+// app.use(bodyParser.urlencoded({ extended: false }))
 
+app.post(
+  "/api/users",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    // console.log(req.body.username)
+    // res.json({ created: true, username: req.body.username, id: req.body.id})
+
+    let newUser = new User({ username: req.body.username })
+    newUser.save((error, data) => {
+      if (!error) {
+        console.log(data.username)
+        let respObj = {}
+        respObj["username"] = data.username
+        respObj["_id"] = data.id
+        res.json(respObj)
+      } else {
+        res.json({ error: "error found!" })
+        console.log(error)
+      }
+    })
+  }
+)
 
 app.use(cors())
 app.use(express.static("public"))
